605b00d53a0dcf05cc362767cf138f8fa76ba53f
tristagirl <tdahleen@me.com>
2022-01-25 13:20:13 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,62 @@
 alert('Hello world')
 
+let PokemonList= [
+  {
+    name:'pikachu',
+    height: 1.4,
+    type:  ['electric' , 'static']
+  },
 
-let favoriteFood = 'Pizza'
-document.write (favoriteFood);
+  {
+    name: 'jigglypuff',
+    height: .7,
+    type: ['cute charm' , 'Competitive']
+  },
+
+  {
+    name: 'Zubat',
+    height: .8,
+    type:['poison' , 'ultrasonic waves']
+  },
+
+  {
+    name:'squirtle',
+    height: .5,
+    type: ['water' , 'Torrent']
+  },
+
+  {
+    name: 'beedrill',
+    height: 1.2,
+    type: ['poison' , 'stings']
+  },
+
+  {
+    name:'arbok',
+    height: .9,
+    type: ['poison' , 'bite']
+  },
+
+  {
+    name:'ninetales',
+    height: 2.1,
+    type: ['fire' , 'flash fire']
+  }
+
+]
+
+console.log(PokemonList);
+
+// issues i am having when using js
+//spacings, and no spacings.
+//remembering to use the ; and '' instead of ""
+
+// In this This Loop + conditional I specify that the pokemon with a height smaller than 1 will have this message
+
+for (var i = 0; i < PokemonList.length; i++) {
+ if (PokemonList[i].height <= 1.0) {
+    document.write('<P>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + ( " (Tinny Pokemon)") + '<P>');
+ } else {
+   document.write('<P>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + '<P>')
+  }
+}
