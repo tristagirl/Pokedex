a5173dd2c5fd99e25628f938c711d2acbbc0b535
tristagirl <tdahleen@me.com>
2022-01-28 11:20:51 -0600

    Update script.js

--- untitled folder/script.js
+++ untitled folder/script.js
@@ -1,146 +1,70 @@
-alert('Hello world')
 
-let pokemonList= [
-  {
-    name:'pikachu',
-    height: 1.4,
-    type:  ['electric' , 'static']
-  },
+let pokemonReposityory = [function ()]
 
-  {
-    name: 'jigglypuff',
-    height: .7,
-    type: ['cute charm' , 'Competitive']
-  },
+      let pokemonList= [
+        {
+          name:'pikachu',
+          height: 1.4,
+          type:  ['electric' , 'static']
+        },
 
-  {
-    name: 'Zubat',
-    height: .8,
-    type:['poison' , 'ultrasonic waves']
-  },
+        {
+          name: 'jigglypuff',
+          height: .7,
+          type: ['cute charm' , 'Competitive']
+        },
 
-  {
-    name:'squirtle',
-    height: .5,
-    type: ['water' , 'Torrent']
-  },
+        {
+          name: 'Zubat',
+          height: .8,
+          type:['poison' , 'ultrasonic waves']
+        },
 
-  {
-    name: 'beedrill',
-    height: 1.2,
-    type: ['poison' , 'stings']
-  },
+        {
+          name:'squirtle',
+          height: .5,
+          type: ['water' , 'Torrent']
+        },
 
-  {
-    name:'arbok',
-    height: .9,
-    type: ['poison' , 'bite']
-  },
+        {
+          name: 'beedrill',
+          height: 1.2,
+          type: ['poison' , 'stings']
+        },
 
-  {
-    name:'ninetales',
-    height: 2.1,
-    type: ['fire' , 'flash fire']
-  }
+        {
+          name:'arbok',
+          height: .9,
+          type: ['poison' , 'bite']
+        },
 
+        {
+          name:'ninetales',
+          height: 2.1,
+          type: ['fire' , 'flash fire']
+        }
 ]
 
-console.log(pokemonList);
 
-// issues i am having when using js
-//spacings, and no spacings.
-//remembering to use the ; and '' instead of ""
+    function getAll () {
+        return pokemonList;
 
-// In this This Loop + conditional I specify that the pokemon with a height greater than 1 will have this message
-
-for (var i = 0; i < pokemonList.length; i++) {
- if (pokemonList[i].height >= 1.0) {
-    document.write('<P>' +  pokemonList[i].name + ( ', height: ' )+ pokemonList[i].height + ( " (Wow, That\'s big!)") + '<P>');
- } else {
-   document.write('<P>' +  pokemonList[i].name + ( ', height: ' )+ pokemonList[i].height + '<P>')
-  }
-}
-
-
-let pokemonRepository = (function () {
-  let pokemonList = [];
-
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
-  function add(pokemon) {
+      }
+  function add (pokemon) {
     pokemonList.push(pokemon);
-  }
+      }
 
-  function getAll() {
-    return pokemonList;
-  }
+      return {
+        getAll: getAll,
+        add: add
+      }
+})()
 
-  function addListItem(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('prettyButton');
-    listItem.appendChild(button);
-    pokemonList.appendChild(listItem);
-    button.addEventListener('click', function () {
-      showDetails(pokemon);
-    });
-  }
-
-  function loadList() {
-    return fetch(apiUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url,
-          };
-          add(pokemon);
-        });
-      })
-      .catch(function (e) {
-        console.error(e);
-      });
-  }
-
-  function loadDetails(item) {
-    let url = item.detailsUrl;
-    return fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (details) {
-        item.imageUrl = details.sprites.front_default;
-        item.height = details.height;
-        item.types = details.types;
-      })
-      .catch(function (e) {
-        console.error(e);
-      });
-  }
-
-  function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
-    });
-  }
-
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails,
-  };
-})();
-
-pokemonRepository.loadList().then(function () {
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
+pokemonList.forEach(function(pokemon) {
+  console.log(pokemonList.name + ' is ' + pokemon.height + 'tall');
 });
+
+userList.forEach(myLoopFunction);
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu'});
+console.log(pokemonRepository.getAll()); // [{ name: 'Pikachu'}]
